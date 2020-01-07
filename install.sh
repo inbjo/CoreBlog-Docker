7c2da0b59ce38ee95ae620caadd00a3fbb3842d2
Flex <2345@mail.com>
2020-01-07 13:44:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,9 +13,13 @@ download_coreblog(){
     wget http://cdn.ikuter.com/CoreBlog/blog.zip
     echo "unzip zip file..."
     unzip -d ./www blog.zip
-    rm -f blog.zip ./www/coreblog/.env
-    cp -f ./www/coreblog/.env.docker ./www/coreblog/.env
-    chmod -R 777 ./www/coreblog/
+    rm -f blog.zip
+    sed -i -e 's|APP_ENV=production|APP_ENV=docker|' ./www/coreblog/.env
+    sed -i -e 's|APP_KEY=|APP_KEY=base64:d0/95VEo40APuttxuy3XSHp+zyNTDMfVY31DRVWBGRs=|' ./www/coreblog/.env
+    sed -i -e 's|JWT_SECRET=|JWT_SECRET=BOTwvn5Cu9XIFT4WLXjg19upFtpWcgLxiNRViyrQw6wTuwbId63Gk2ca8fmbuJET|' ./www/coreblog/.env
+    sed -i -e 's|CACHE_DRIVER=file|CACHE_DRIVER=redis|' ./www/coreblog/.env
+    sed -i -e 's|SESSION_DRIVER=file|SESSION_DRIVER=redis|' ./www/coreblog/.env
+    chmod -R 777 ./www/coreblog/.env ./www/coreblog/storage ./www/coreblog/bootstrap/cache/
     echo "Install Success"
 }
 download_coreblog
