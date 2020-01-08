9adc119d82885b603e54622aa94bd0aca96db7cf
Flex <2345@mail.com>
2020-01-08 16:31:38 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,14 @@ root_need() {
 }
 root_need
 
+command -v wget >/dev/null 2>&1 || { echo >&2 "I require wget but it's not installed.  Aborting."; exit 1; }
+command -v tar >/dev/null 2>&1 || { echo >&2 "I require tar but it's not installed.  Aborting."; exit 1; }
+
 download_coreblog(){
-    echo "Start Downloading CoreBlog..."
-    wget http://cdn.ikuter.com/CoreBlog/blog.zip
-    echo "unzip zip file..."
-    unzip -d ./www blog.zip
-    rm -f blog.zip
+    echo "Downloading CoreBlog..."
+    wget http://cdn.ikuter.com/CoreBlog/blog.tar.gz
+    tar -zxvf blog.tar.gz -C ./www
+    rm -f blog.tar.gz
     sed -i -e 's|APP_ENV=production|APP_ENV=docker|' ./www/coreblog/.env
     sed -i -e 's|APP_KEY=|APP_KEY=base64:d0/95VEo40APuttxuy3XSHp+zyNTDMfVY31DRVWBGRs=|' ./www/coreblog/.env
     sed -i -e 's|JWT_SECRET=|JWT_SECRET=BOTwvn5Cu9XIFT4WLXjg19upFtpWcgLxiNRViyrQw6wTuwbId63Gk2ca8fmbuJET|' ./www/coreblog/.env
